5b6508e185aae8466cbbe5a65a89523c45ae758c
kaylommy <kaylajfreeman13@gmail.com>
2024-02-21 00:10:07 -0500

    added a while loop and a foor loop and if statements

--- Develop/script.js
+++ Develop/script.js
@@ -5,22 +5,56 @@ function generatePassword() {
   var passwordLength = prompt("Please select a number between 8 and 128 for the length of your password.")
 
   while (passwordLength < 8 || passwordLength > 128) {
-      window.alert("The number selected was not between 8 and 128. Please try again.")
-      passwordLength = prompt("Please select a number between 8 and 128 for the length of your password");
+    window.alert("The number selected was not between 8 and 128. Please try again.")
+    passwordLength = prompt("Please select a number between 8 and 128 for the length of your password");
   }
 
-  upper = confirm("Would you like your password to have uppercase letters?")
-  lower = confirm("Would you like your password to have lowercase letters?")
-  special = confirm("Would you like your password to have special characters?")
-  number = confirm("Would you like your password to contain numbers?")
+  var upper = false
+  var lower = false
+  var special = false
+  var number = false
+  while (upper === false && lower === false && special === false && number === false) {
+
+    upper = confirm("Would you like your password to have uppercase letters?")
+    lower = confirm("Would you like your password to have lowercase letters?")
+    special = confirm("Would you like your password to have special characters?")
+    number = confirm("Would you like your password to contain numbers?")
+
+    if (upper === false && lower === false && special === false && number === false) {
+      window.alert("You must select at least one option. Please try again.")
+    }
+  }
+
+  var computerPick = ""
+  for (i = 0; i < passwordLength; i++) {
+
+    if (upper && computerPick.length < passwordLength) {
+      var randomIndex = Math.floor(Math.random() * uppercase.length)
+      computerPick = computerPick + uppercase[randomIndex]
+    }
+
+    if (lower && computerPick.length < passwordLength) {
+      var randomIndex = Math.floor(Math.random() * lowercase.length)
+      computerPick = computerPick + lowercase[randomIndex]
+    }
+    if (special && computerPick.length < passwordLength) {
+      var randomIndex = Math.floor(Math.random() * specialcharacter.length)
+      computerPick = computerPick + specialcharacter[randomIndex]
+    }
+    if (number && computerPick.length < passwordLength) {
+      var randomIndex = Math.floor(Math.random() * digits.length)
+      computerPick = computerPick + digits[randomIndex]
+    }
+  }
+  return computerPick;
 
 }
 
 // variables for passowrd characters
-var upper = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var special = ['!', '@', '#', '$', '%', '&', '*', '+', '=', ";", ':', '?', ',', '.', '/', '<', '>', '{', '}', '[', ']', '(', ')', '~', '^'];
-var number = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialcharacter = ['!', '@', '#', '$', '%', '&', '*', '+', '=', ";", ':', '?', ',', '.', '/', '<', '>', '{', '}', '[', ']', '(', ')', '~', '^'];
+var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
