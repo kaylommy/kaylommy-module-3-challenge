85be58fa06e5b3fd891cfc6e941d4c2fb9d49776
kaylommy <kaylajfreeman13@gmail.com>
2024-02-21 22:43:38 -0500

    finished README and added a couple comments in JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,9 @@
 // Assignment code here
+// variables for passowrd characters
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialcharacter = ['!', '@', '#', '$', '%', '&', '*', '+', '=', ";", ':', '?', ',', '.', '/', '<', '>', '{', '}', '[', ']', '(', ')', '~', '^'];
+var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 function generatePassword() {
   //prompt user for password criteria
@@ -19,12 +24,12 @@ function generatePassword() {
     lower = confirm("Would you like your password to have lowercase letters?")
     special = confirm("Would you like your password to have special characters?")
     number = confirm("Would you like your password to contain numbers?")
-
+    // if all are false then it will loop to the options again
     if (upper === false && lower === false && special === false && number === false) {
       window.alert("You must select at least one option. Please try again.")
     }
   }
-
+  // computerPick is left blank so we can have it assigned to a new value each if statement.
   var computerPick = ""
   for (i = 0; i < passwordLength; i++) {
 
@@ -49,13 +54,6 @@ function generatePassword() {
   return computerPick;
 
 }
-
-// variables for passowrd characters
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var specialcharacter = ['!', '@', '#', '$', '%', '&', '*', '+', '=', ";", ':', '?', ',', '.', '/', '<', '>', '{', '}', '[', ']', '(', ')', '~', '^'];
-var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Password Generator Starter Code
+
+## Description
+
+The purpose of this project was to create with JavaScript a password generator. In the code there is first variables of uppercase, lowercase, special characters, and numbers, these are selectable criteria for passwords. The final application is used as a random password generator within user selected criteria.
+
+## Screenshot
+![Password-Generator](assets/password-generator.jpg "Website")
+
+## Website
+
+## Note/Contributors
+
+I had worked on this project with Michael Freeman.
